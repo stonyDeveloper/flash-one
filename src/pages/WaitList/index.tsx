974bf5138c35484ab5d9b74f05e4084933aa471d
stonyDeveloper <stoneachi@gmail.com>
2024-10-18 12:51:18 +0100

    waitlist design conversion

--- src/pages/WaitList/index.tsx
+++ src/pages/WaitList/index.tsx
@@ -1,49 +1,38 @@
 import Logo from "./view/Logo";
-import BoxOne from "../../../src/assets/box_1.svg";
-import AvatarOne from "../../../src/assets/avatar_1.jpg";
-import AvatarTwo from "../../../src/assets/avata_2.jpg";
-import AvatarThree from "../../../src/assets/avatar_3.png";
 import AvatarGroup from "../../../src/assets/_avatar group_.svg";
 import Heart from "../../../src/assets/heart.svg";
 import Cross from "../../../src/assets/medical_cross.svg";
-import Planet from "../../../src/assets/planet.png"
+import Planet from "../../../src/assets/planet.png";
+import X from "../../../src/assets/twitter.svg";
+import Instagram from "../../../src/assets/instagram.svg";
+import Linkedin from "../../../src/assets/linkedin.svg";
+import Facebook from "../../../src/assets/facebook.svg";
+
+const media_icons = [X, Facebook, Linkedin, Instagram];
 
 const WaitList = () => {
   return (
-    <div className="waitlist h-screen relative w-screen overflow-hidden">
+    <div className="waitlist relative w-screen">
       <div className="grid place-items-center pt-10">
         <Logo />
       </div>
 
-      <img className="mt-6" src={BoxOne} alt="" />
-
-      <div className="flex items-center gap-6 mt-6">
+      <div className="flex items-center gap-6 mt-20">
         <div className="w-full h-[0.5px] waitlist-line-left"></div>
         <p className="text-[16px] bg-text-gradient bg-clip-text text-transparent">
           Waitlist
         </p>
         <div className="w-full h-[0.5px] waitlist-line-right transform scale-x-[-1]"></div>
       </div>
-      <div className="grid place-items-center">
-        <div className="line-with-circles w-[calc(100vw-1.9rem)]   z-50 mt-[54px]"></div>
-      </div>
 
-      <div className="text-[48px] font-cabinet font-extrabold bg-text-gradient bg-clip-text text-transparent text-center z-50">
+      <div className="text-[48px] font-cabinet font-extrabold bg-text-gradient bg-clip-text text-transparent text-center z-50 mt-10">
         FlashOne Health
       </div>
 
-      <div className="grid place-items-center">
-        <div className="line-with-circles w-[calc(100vw-1.9rem)] z-50"></div>
-      </div>
-
-      <p className="text-center mx-auto mt-6 px-8 tracking-tight leading-[18px] bg-text-gradient bg-clip-text text-transparent font-[400]">
+      <p className="text-center mx-auto mt-2 px-8 tracking-tighter leading-[20px] bg-text-gradient bg-clip-text text-transparent font-[400]">
         Join our waitlist for exclusive access to top-tier health insurance
         plans for your family in Nigeria.
       </p>
-
-      <div className="grid place-items-center mt-6">
-        <div className="line-with-circles w-[calc(100vw-1.9rem)]   z-50"></div>
-      </div>
 
       <div className="px-8 mt-6">
         <input
@@ -61,29 +50,40 @@ const WaitList = () => {
         <img src={AvatarGroup} alt="" />
       </div>
 
-      <p className="text-center mx-auto mt-4 px-8 tracking-tight leading-[18px] bg-text-gradient bg-clip-text text-transparent font-[400]">
+      <p className="text-center mx-auto mt-2 px-8 tracking-tight leading-[18px] bg-text-gradient bg-clip-text text-transparent font-[400]">
         Join others in the diaspora already onboard!
       </p>
 
-      <div className="grid place-items-center mt-2">
-        <div className="line-with-circles w-[calc(100vw-1.9rem)]   z-50"></div>
-      </div>
-
-      <div className="pl-4">
-        
-        <div className="px-1 border-r w-fit">
+      <div className="w-full relative">
+        <div className="w-fit absolute top-6 space-y-2 pl-6">
           <img src={Heart} alt="heart" />
           <img src={Cross} alt="cross" />
+          <img src={Heart} alt="heart" />
+        </div>
+        <div className="w-fit absolute top-6 space-y-2 right-0 pr-6">
+          <img src={Heart} alt="heart" />
+          <img src={Cross} alt="cross" />
+          <img src={Heart} alt="heart" />
         </div>
       </div>
 
-      <img  className="w-full z-50" src={Planet} alt="" />
-
-      <div className="absolute bottom-0 left-4  grid place-items-center h-screen z-10">
-        <div className="h-[calc(100vh-3.125rem)] w-[0.5px] bg-custom-gradient"></div>
+      <div className="relative">
+        <img className="w-full" src={Planet} alt="Planet" />
+        <div className="absolute top-24 inset-0 flex justify-center items-center">
+          <div className="flex gap-8">
+            {media_icons.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Icon ${index + 1}`}
+                className="w-8 h-8"
+              />
+            ))}
+          </div>
+        </div>
       </div>
-      <div className="absolute bottom-0 right-4  grid place-items-center h-screen z-10">
-        <div className="h-[calc(100vh-3.125rem)] w-[0.5px] bg-custom-gradient"></div> 
+      <div className="footer text-[12px] font-[400] text-[#13949A] py-2 text-center border-t">
+        © 2024, FlashOne Health. All Rights Reserved
       </div>
     </div>
   );
