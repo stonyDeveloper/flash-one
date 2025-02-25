d28d4daa4f2088781dd3fe180e8bed3d92581036
stonyDeveloper <stoneachi@gmail.com>
2025-02-25 20:43:58 +0100

    chore: update brand name

--- src/pages/WaitList/index.tsx
+++ src/pages/WaitList/index.tsx
@@ -6,6 +6,7 @@ import SocialMediaIcons from "./view/SocialMediaIcons";
 import NavigationLinks from "./view/NavigationLinks";
 // dont forget to remove h-screen x
 const WaitList = () => {
+  const year = new Date().getFullYear()
   return (
     <div className="waitlist relative "> 
       <div className="grid place-items-center pt-10 lg:hidden">
@@ -26,7 +27,7 @@ const WaitList = () => {
       <SocialMediaIcons />
 
       <div className="footer text-[12px] lg:text-[14px] font-[400] lg:font-[500] text-[#13949A] py-2 lg:py-4 lg:pt-5 text-center border-t  bg-[#0E292B]  z-50 absolute  bottom-0 w-full -mt-1 flex justify-center items-center gap-[100px]">
-        <p>© 2024, FlashOne Health. All Rights Reserved</p>
+        <p>© {year}, WeCareHealth Health. All Rights Reserved</p>
         <NavigationLinks />
       </div>
     </div>
